7e1f631dfa037717087c09ffaf0e70afa9c80072
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-05-22 08:55:39 -0500

    2nd way.

--- assets/buttonjs.js
+++ assets/buttonjs.js
@@ -1,13 +1,16 @@
 var button = document.querySelector("button");
 var body = document.querySelector("body");
-var isPurple =false;
+// var isPurple =false;
 
+// button.addEventListener("click", function () {
+//     if(isPurple) {
+//         body.style.backgroundColor = "white"
+//         isPurple = false;
+//     }else {   
+//         body.style.backgroundColor = "purple"
+//         isPurple = true;
+//     }
+// });
 button.addEventListener("click", function () {
-    if(isPurple) {
-        body.style.backgroundColor = "white"
-        isPurple = false;
-    }else {   
-        body.style.backgroundColor = "purple"
-        isPurple = true;
-    }
+    body.classList.toggle("purple");
 });
