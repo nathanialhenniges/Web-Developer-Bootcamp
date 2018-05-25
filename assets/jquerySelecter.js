ef2f5469fcfe429e4d1d3d82cbe6a7c717b7e35d
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-05-25 10:42:43 -0500

    More.

---
+++ assets/jquerySelecter.js
@@ -0,0 +1,5 @@
+$("div").css("background", "purple")
+$(".hightlight").css("width", "200px")
+// $("div.hightlight").css("width", "200px")
+$("#third").css("border", "2px dashed orange")
+$("div:first-of-type").css("color", "pink")
