f486b3dec3bbdf0ea9f3381a3013c2ec02880e04
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-01 08:27:37 -0500

    Working code.

--- assets/jqeryTodolist.js
+++ assets/jqeryTodolist.js
@@ -1,4 +1,19 @@
 // Check off todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
+})
+// Click X to remove todo
+$("ul").on("click" , "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove()
+    });
+    event.stopPropagation();
+})
+$("input[type='text'").keypress(function (e) {
+    if (e.which === 13) {
+        var todoText = $(this).val()
+        var todoTextFixed = todoText.replace(/(<([^>]+)>)/ig, "");
+        $(this).val(" ");
+        $("ul").append("<li>" + "<span>X</span>" + todoTextFixed + "</li>")
+    }
 })
