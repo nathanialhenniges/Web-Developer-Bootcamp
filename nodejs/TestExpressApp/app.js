691e869ffa3af30048662e188c4e27569624885c
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-19 08:10:13 -0500

    More.

---
+++ nodejs/TestExpressApp/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var app = express();
+
+
+/**
+ * R
+ */
+
+app.get("/", function (req, res) {
+    res.send("Hi there!")
+})
+app.get("/bye", function (req, res){
+    res.send("Bye")
+})
+app.get("/dog", function (req, res) {
+    res.send("Meow!")
+})
+/**
+ * Start server
+ */
+app.listen("8080", "127.0.0.1", function () {
+    console.log("Server has started!")
+});
+console.log("Go to http://127.0.0.1:8080")
