6f07c1048b82b00f212831a96ba9fedb81f8c846
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-19 10:01:54 -0500

    More.

--- nodejs/TestExpressApp/app.js
+++ nodejs/TestExpressApp/app.js
@@ -9,19 +9,26 @@ var app = express();
 app.get("/", function (req, res) {
     res.send("Hi there!")
 })
-app.get("/bye", function (req, res){
+app.get("/bye", function (req, res) {
     res.send("Bye")
 })
 app.get("/dog", function (req, res) {
     res.send("Meow!")
 })
-app.get("*", function(req, res) {
+app.get("/r/:subredditName", function (req, res) {
+    var subreddit = req.params.subredditName
+    res.send("Welcome to the " + subreddit.toUpperCase() + " reddit")
+})
+app.get("/r/:subredditName/commands/:id/:title", function (req, res) {
+    res.send("Welcome to the comments page.")
+})
+app.get("*", function (req, res) {
     res.send("You are a star!")
 })
 /**
  * Start server
  */
-app.listen("8080", "127.0.0.1", function () {
+app.listen("8080", function () {
     console.log("Server has started!")
 });
 console.log("Go to http://127.0.0.1:8080")
