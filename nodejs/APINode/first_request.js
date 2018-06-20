74f27c028af8f012eaef0a12e0239ce45e420d0f
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-20 10:16:56 -0500

    API Coggers.

--- nodejs/APINode/first_request.js
+++ nodejs/APINode/first_request.js
@@ -1,6 +1,8 @@
 const request = require("request");
-request('https://www.google.com', function (err, res, body) {
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (err, res, body) {
     if (!err && res.statusCode == 200) {
-        console.log(body)
+        var parseData = JSON.parse(body)
+        var sunset = parseData.query.results.channel.astronomy.sunset
+        console.log("Sunset in Hawaii is at " + sunset)
     }
 })
