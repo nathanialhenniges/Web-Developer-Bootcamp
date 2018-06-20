3a5e1090bd06e07fd013c2ce4922353055fe0006
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-20 09:27:33 -0500

    More fun time.

--- nodejs/PostRequestExpress/app.js
+++ nodejs/PostRequestExpress/app.js
@@ -2,24 +2,33 @@
  * Setup Lib
  */
 const express = require("express");
-
-
+const bodyParser = require("body-parser")
 var app = express();
 /**
  * Assets
  */
 app.use(express.static('public'))
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 /**
  * Setup routes
  */
+var friends = ["Holy", "Nathan", "Bob", "Tony", "Steven"]
 app.get("/", function (req, res) {
     res.render("home");
 })
 app.get("/friends", function (req, res) {
-    var friends = ["Holy", "Nathan", "Bob", "Tony", "Steven"]
-    res.render("friends", {friends: friends})
+    res.render("friends", {
+        friends: friends
+    })
 
+})
+app.post("/addfriend", function (req, res) {
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend)
+res.redirect("/friends")
 })
 /**
  * Start server
