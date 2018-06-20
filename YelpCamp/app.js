b0cbdd64d79a50e22ee92d4ec2da96592c82b6ea
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-20 15:14:06 -0500

    Added yelpcamp.

---
+++ YelpCamp/app.js
@@ -0,0 +1,49 @@
+/**
+ * Import Node Modules
+ */
+const express = require('express')
+
+/**
+ * Setup Express
+ */
+const app = express();
+/**
+ * Assets
+ */
+app.use(express.static('public'))
+/**
+ * Set global View
+ */
+app.set("view engine", "ejs");
+/**
+/**
+ * Setup routes
+ */
+app.get("/", function (req, res) {
+    res.render("home");
+})
+app.get("/campgrounds", function (req, res) {
+    /**
+     * Campgrounds array
+     */
+    var campgrounds = [{
+            name: "Salmon Creek",
+            image: "https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104497f9c478a6e5b1ba_340.jpg"
+        },
+        {
+            name: "Cat Creek",
+            image: "https://pixabay.com/get/e136b60d2af51c22d2524518b7444795ea76e5d004b014439cf5c078afe9b7_340.jpg"
+        }, {
+            name: "Cat Dog Campgrounds",
+            image: "https://pixabay.com/get/e831b20628f2003ed1584d05fb1d4e97e07ee3d21cac104497f9c478a6e5b1ba_340.jpg"
+        }
+    ]
+    res.render("campgrounds");
+})
+/**
+ * Start server
+ */
+app.listen("8080", function () {
+    console.log("Server has started!")
+});
+console.log("Go to http://localhost:8080 to see running app.")
