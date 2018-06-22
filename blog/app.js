e6b3af108d7f6c21ccbe8f4ae2fb878ec0bb18fd
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-22 18:01:23 -0500

    more.

--- blog/app.js
+++ blog/app.js
@@ -68,15 +68,26 @@ app.get("/blogs", function (req, res) {
         }
     })
 });
-app.get("/blogs/new" ,function (req,res){
+app.post("/blogs", function (req, res) {
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs")
+        }
+    })
+})
+app.get("/blogs/new", function (req, res) {
     res.render("new")
 })
-app.post("/blogs", function(req,res) {
-    Blog.create(req.body.blog,function(err,newBlog){
-        if(err){
-            res.render("new");
-        }else {
-            res.redirect("/blogs")
+app.get("/blogs/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect('/blogs')
+        } else {
+            res.render("show", {
+                blog: foundBlog
+            })
         }
     })
 })
