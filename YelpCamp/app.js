995996fde4c6ccd53ce96352ff992c0cbca1a54f
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-22 08:01:06 -0500

    More.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost/yelpcamp")
  */
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 })
 
 var Campground = mongoose.model("Campground", campgroundSchema)
@@ -41,7 +42,8 @@ var Campground = mongoose.model("Campground", campgroundSchema)
  */
 // Campground.create({
 //     name: "Cat Dog Campgrounds",
-//     image: "https://pixabay.com/get/e831b20628f2003ed1584d05fb1d4e97e07ee3d21cac104497f9c478a6e5b1ba_340.jpg"
+//     image: "https://pixabay.com/get/ea31b10929f7063ed1584d05fb1d4e97e07ee3d21cac104497f9c77ea6e8b5ba_340.jpg",
+//     description: "This is just another campground but with cat dog themed areas."
 // }, function (err, campground) {
 //     if (err) {
 //         console.log(err);
@@ -55,7 +57,7 @@ var Campground = mongoose.model("Campground", campgroundSchema)
  */
 app.get("/", function (req, res) {
     res.render("home");
-})
+});
 app.get("/campgrounds", function (req, res) {
     Campground.find({}, function (err, campgrounds) {
         if (err) {
@@ -66,13 +68,15 @@ app.get("/campgrounds", function (req, res) {
             });
         }
     })
-})
+});
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description
     var newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     };
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -84,8 +88,17 @@ app.post("/campgrounds", function (req, res) {
     })
 });
 app.get("/campgrounds/new", function (req, res) {
-    res.render("newCampground")
-})
+    res.render("newCampground");
+});
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("showCampground" , {campground: foundCampground});
+        }
+    });
+});
 /**
  * Start server
  */
