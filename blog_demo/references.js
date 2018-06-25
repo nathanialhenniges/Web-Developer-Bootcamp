1bd3867067aeec322d8593e3e1ccc363db7ac8c0
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-25 06:31:29 -0500

    Had to do two times to get working must have typo.

--- blog_demo/references.js
+++ blog_demo/references.js
@@ -10,6 +10,43 @@ var Post = mongoose.model("Post", postSchema);
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    posts: [postSchema]
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }]
 })
 var User = mongoose.model("User", userSchema);
+
+// User.create({
+//     email: "mrdemonwolf@outlook.com",
+//     name: "Bob"
+// })
+// Post.create({
+//         title: "Apples part 3.",
+//         content: "nfjjelngegeglkgekeglkneggenngeegegknklgelgenkgelnknlegka"
+//     },
+//     function (err, post) {
+//         User.findOne({
+//             email: "mrdemonwolf@outlook.com"
+//         }, function (err, foundUser) {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 foundUser.posts.push(post);
+//                 foundUser.save(function (err, data) {
+//                     if (err) {
+//                         console.log(err)
+//                     } else {
+//                         console.log(data)
+//                     }
+//                 })
+//             }
+//         })
+//     })
+User.findOne({email: "mrdemonwolf@outlook.com"}).populate("posts").exec(function (err, user) {
+if(err) {
+    console.log(err)
+}else {
+    console.log(user.email)
+}
+})
