3fc26f149fe2191d42d7de3ea94a458d2f87eb7f
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-26 07:51:09 -0500

    First.

---
+++ blog_demo/models/post.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+})
+var Post = mongoose.model("Post", postSchema);
+
+module.exports = Post
--- blog_demo/references.js
+++ blog_demo/references.js
@@ -1,52 +1,37 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo")
-
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-})
-var Post = mongoose.model("Post", postSchema);
-
-var userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-})
-var User = mongoose.model("User", userSchema);
-
+var User = require("./models/user")
+var Post = require("./models/post")
 // User.create({
 //     email: "mrdemonwolf@outlook.com",
 //     name: "Bob"
 // })
-// Post.create({
-//         title: "Apples part 3.",
-//         content: "nfjjelngegeglkgekeglkneggenngeegegknklgelgenkgelnknlegka"
-//     },
-//     function (err, post) {
-//         User.findOne({
-//             email: "mrdemonwolf@outlook.com"
-//         }, function (err, foundUser) {
-//             if (err) {
-//                 console.log(err)
-//             } else {
-//                 foundUser.posts.push(post);
-//                 foundUser.save(function (err, data) {
-//                     if (err) {
-//                         console.log(err)
-//                     } else {
-//                         console.log(data)
-//                     }
-//                 })
-//             }
-//         })
-//     })
-User.findOne({email: "mrdemonwolf@outlook.com"}).exec(function (err, user) {
-if(err) {
-    console.log(err)
-}else {
-    console.log(user.name)
-}
-})
+Post.create({
+        title: "Apples part 4.",
+        content: "fefefeegggegergegeewguigeuiogeioiuohfegwiohegiohnegin"
+    },
+    function (err, post) {
+        User.findOne({
+            email: "mrdemonwolf@outlook.com"
+        }, function (err, foundUser) {
+            if (err) {
+                console.log(err)
+            } else {
+                foundUser.posts.push(post);
+                foundUser.save(function (err, data) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(data)
+                    } 
+                })
+            }
+        })
+    })
+// User.findOne({email: "mrdemonwolf@outlook.com"}).exec(function (err, user) {
+// if(err) {
+//     console.log(err)
+// }else {
+//     console.log(user)
+// }
+// })
