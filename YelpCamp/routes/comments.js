e733753fd2e358162b00e19c6a49d5136b60a259
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-06-28 09:23:56 -0500

    comment edit works.

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -39,10 +39,31 @@ router.post("/", isLoggedIn, function (req, res) {
                     comment.save();
                     campground.comments.push(comment);
                     campground.save();
-                    console.log(comment)
                     res.redirect("/campgrounds/" + campground._id)
                 }
             });
+        }
+    });
+});
+router.get("/:comment_id/edit", function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", {
+                campground_id: req.params.id,
+                comment: foundComment
+            })
+
+        }
+    })
+})
+router.put("/:comment_id", function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
         }
     });
 });
